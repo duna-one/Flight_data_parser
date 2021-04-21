03a006e6ac2541736f7905b6619669c13afa658e
Даниил <korotkovdan@outlook.com>
2021-04-21 20:14:15 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Prac4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Prac4.Properties.Resources._6504903ec685145a9237d4466aabaa16;
+            this.pictureBox1.Location = new System.Drawing.Point(355, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Найти и вывести";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(119, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Номер рейса";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "dd.MM.yyyy  HH:mm:ss";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(140, 25);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(209, 20);
+            this.dateTimePicker1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(140, 51);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(209, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Найти и вывести координаты";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(137, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(212, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Уточнить координату по дате и времени";
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(15, 80);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(334, 318);
+            this.listView1.TabIndex = 8;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(865, 518);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Задание 4";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using Prac4.FlyAPI;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Prac4
+{
+    public partial class Form1 : Form
+    {
+        FlightXML2SoapClient FlyInformator = new FlightXML2SoapClient();
+        public Form1()
+        {
+            Cursor.Current = Cursors.AppStarting;
+            InitializeComponent();
+            FlyInformator.ClientCredentials.UserName.UserName = "DunaOne";
+            FlyInformator.ClientCredentials.UserName.Password = "5e662b11de4745e744970c5b3fb94cfa578208f8";
+            FlyInformator.SetMaximumResultSize(1);
+            Cursor.Current = Cursors.Default;
+        }
+
+        public Image StringToImage(string base64String)
+        {
+            if (String.IsNullOrWhiteSpace(base64String))
+                return null;
+
+            var bytes = Convert.FromBase64String(base64String);
+            var stream = new MemoryStream(bytes);
+            return Image.FromStream(stream);
+
+        }
+
+        public void FlyInfoToString(FlightStruct flightStruct)
+        {
+            listView1.Items.Add("Aircraft Type: " + flightStruct.aircrafttype);
+            listView1.Items.Add("Ident: " + flightStruct.ident);
+            listView1.Items.Add("Destination: " + flightStruct.destination);
+            listView1.Items.Add("Origin: " + flightStruct.origin);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+            try
+            {
+                listView1.Items.Clear();
+                FlyInfoToString(FlyInformator.FlightInfo(textBox1.Text, 1).flights[0]);
+                pictureBox1.Image = StringToImage(FlyInformator.MapFlight(textBox1.Text, 500, 500));
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show("Некорректный ввод", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            Cursor.Current = Cursors.Default;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                TrackStruct[] tracks = FlyInformator.GetLastTrack(textBox1.Text);
+
+                DateTime dt = TimeZoneInfo.ConvertTimeToUtc(dateTimePicker1.Value);
+                DateTime dt1970 = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+                TimeSpan tsInterval = dt.Subtract(dt1970);
+                Int32 iSeconds = Convert.ToInt32(tsInterval.TotalSeconds);
+
+                for (int i = 1; i < tracks.Length; i++)
+                {
+                    if (tracks[i].timestamp > iSeconds && tracks[i - 1].timestamp < iSeconds)
+                    {
+                        listView1.Items.Add("Широта: " + tracks[i].latitude);
+                        listView1.Items.Add("Долгота: " + tracks[i].longitude);
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Некорректный ввод", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
